65f30facdb5c9995a2e35a84f5c340e24749e822
Dmytro Voloskiy <dmytrovoloskiy123@gmail.com>
2020-12-11 12:55:03 +0200

    react articles api client

--- src/client/my-blog-client/src/api-client.ts
+++ src/client/my-blog-client/src/api-client.ts
@@ -1,5 +1,16 @@
-export const API_BASE_URL = ""  //web api base url
+import axios from 'axios';
+import { ICreateArticleCommand } from './models/Articles/Commands';
 
 
+export const API_BASE_URL = "http://localhost:5001/"  //web api base url
 
+
+export class ArticlesClient {
+   public async create(command: ICreateArticleCommand) {
+      const url = `${API_BASE_URL}/api/article`
+      const response = await axios.post<number>(url, command);
+      console.log(response.data);
+      return response.data;
+   }
+}
 
--- src/client/my-blog-client/src/components/App.tsx
+++ src/client/my-blog-client/src/components/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { ArticlesClient } from '../api-client';
 
-
+const articleClient = new ArticlesClient();
+const result = articleClient.create({ title: "Client Test Article", content: "some data" });
+console.log(result);
 function App() {
   return (
     <div>
-      <span>text</span>
+
     </div>
   );
 }
