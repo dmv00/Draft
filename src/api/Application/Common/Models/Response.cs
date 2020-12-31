2c58772c6bfe8bad1a0a5d9d994ac4bc7d1c7138
Dmytro Voloskiy <dmytrovoloskiy123@gmail.com>
2020-12-31 14:39:18 +0200

    response type errors fix

--- src/api/Application/Common/Models/Response.cs
+++ src/api/Application/Common/Models/Response.cs
@@ -3,35 +3,46 @@ using System.Threading.Tasks;
 
 namespace Application.Common.Models
 {
-   public static class Response
-   {
-      public static Response<object> Fail(string message, IEnumerable<string> error) =>
-         new Response<object>(default, message, error);
-      public static Response<object> Fail(string message, string error) =>
-         new Response<object>(default, message, error);
-      public static Response<T> Ok<T>(T data, string message) =>
-         new Response<T>(data, message);
-      public static Task<Response<T>> OkFromTask<T>(T data, string message) =>
-         Task.FromResult(new Response<T>(data, message));
-   }
-   public class Response<T>
-   {
-      public Response(T data, string msg, IEnumerable<string> errors)
-      {
-         Data = data;
-         Message = msg;
-         Errors = new List<string>(errors);
-      }
-      public Response(T data, string msg, string errors = null)
-      {
-         Data = data;
-         Message = msg;
-         Errors = new List<string>(new []{errors});
-      }
+  public static class Response
+  {
+    public static Response<object> Fail(string message, IEnumerable<string> error) =>
+      new Response<object>(default, message, error);
 
-      public T Data { get; set; }
-      public string Message { get; set; }
-      public List<string> Errors { get; }
+    public static Response<object> Fail(string message, string error) =>
+      new Response<object>(default, message, error);
 
-   }
+    public static Response<T> Ok<T>(T data, string message) =>
+      new Response<T>(data, message);
+
+    public static Task<Response<T>> OkFromTask<T>(T data, string message) =>
+      Task.FromResult(new Response<T>(data, message));
+  }
+
+  public class Response<T>
+  {
+    private Response()
+    {
+      Errors = new List<string>();
+    }
+
+    public Response(T data, string msg, IEnumerable<string> errors)
+      : this()
+    {
+      Data = data;
+      Message = msg;
+      Errors.AddRange(errors);
+    }
+
+    public Response(T data, string msg, string error = null)
+      : this()
+    {
+      Data = data;
+      Message = msg;
+      if (error != null) Errors.Add(error);
+    }
+
+    public T Data { get; set; }
+    public string Message { get; set; }
+    public List<string> Errors { get; }
+  }
 }
--- src/api/WebApi/Filters/ApiExceptionFilterAttribute .cs
+++ src/api/WebApi/Filters/ApiExceptionFilterAttribute .cs
@@ -9,96 +9,96 @@ using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace WebApi.Filters
 {
-   public class ApiExceptionFilterAttribute : ExceptionFilterAttribute
+  public class ApiExceptionFilterAttribute : ExceptionFilterAttribute
+  {
+    private readonly IDictionary<Type, Action<ExceptionContext>> _exceptionHandlers;
+
+    public ApiExceptionFilterAttribute()
     {
-        private readonly IDictionary<Type, Action<ExceptionContext>> _exceptionHandlers;
-
-        public ApiExceptionFilterAttribute()
-        {
-            // Register known exception types and handlers.
-            _exceptionHandlers = new Dictionary<Type, Action<ExceptionContext>>
-            {
-                {typeof(ValidationException), HandleValidationException},
-                {typeof(NotFoundException), HandleNotFoundException},
-            };
-        }
-
-        public override void OnException(ExceptionContext context)
-        {
-            HandleException(context);
-
-            base.OnException(context);
-        }
-
-        private void HandleException(ExceptionContext context)
-        {
-            Type type = context.Exception.GetType();
-            if (_exceptionHandlers.ContainsKey(type))
-            {
-                _exceptionHandlers[type].Invoke(context);
-                return;
-            }
-
-            if (!context.ModelState.IsValid)
-            {
-                HandleInvalidModelStateException(context);
-                return;
-            }
-
-            HandleUnknownException(context);
-        }
-
-        private void HandleUnknownException(ExceptionContext context)
-        {
-            var response = Response.Fail(
-                "An error occurred while processing your request."
-                , "Internal server error");
-
-            context.Result = new ObjectResult(response)
-            {
-                StatusCode = StatusCodes.Status500InternalServerError
-            };
-
-            context.ExceptionHandled = true;
-        }
-
-        private void HandleValidationException(ExceptionContext context)
-        {
-            var exception = context.Exception as ValidationException;
-
-            var errors = exception.Errors.SelectMany(err => err.Value);
-
-            var response = Response.Fail(
-                "Validation error",
-                errors);
-
-            context.Result = new BadRequestObjectResult(response);
-
-            context.ExceptionHandled = true;
-        }
-
-        private void HandleInvalidModelStateException(ExceptionContext context)
-        {
-            var response = Response.Fail(
-                string.Empty,
-                "Invalid model state");
-
-            context.Result = new BadRequestObjectResult(response);
-
-            context.ExceptionHandled = true;
-        }
-
-        private void HandleNotFoundException(ExceptionContext context)
-        {
-            var exception = context.Exception as NotFoundException;
-
-            var response = Response.Fail(
-                "The specified resource was not found.",
-                exception.Message);
-
-            context.Result = new NotFoundObjectResult(response);
-
-            context.ExceptionHandled = true;
-        }
+      // Register known exception types and handlers.
+      _exceptionHandlers = new Dictionary<Type, Action<ExceptionContext>>
+      {
+        {typeof(ValidationException), HandleValidationException},
+        {typeof(NotFoundException), HandleNotFoundException},
+      };
     }
+
+    public override void OnException(ExceptionContext context)
+    {
+      HandleException(context);
+
+      base.OnException(context);
+    }
+
+    private void HandleException(ExceptionContext context)
+    {
+      Type type = context.Exception.GetType();
+      if (_exceptionHandlers.ContainsKey(type))
+      {
+        _exceptionHandlers[type].Invoke(context);
+        return;
+      }
+
+      if (!context.ModelState.IsValid)
+      {
+        HandleInvalidModelStateException(context);
+        return;
+      }
+
+      HandleUnknownException(context);
+    }
+
+    private void HandleUnknownException(ExceptionContext context)
+    {
+      var response = Response.Fail(
+        "An error occurred while processing your request."
+        , "Internal server error");
+
+      context.Result = new ObjectResult(response)
+      {
+        StatusCode = StatusCodes.Status500InternalServerError
+      };
+
+      context.ExceptionHandled = true;
+    }
+
+    private void HandleValidationException(ExceptionContext context)
+    {
+      var exception = context.Exception as ValidationException;
+
+      var errors = exception.Errors.SelectMany(err => err.Value);
+
+      var response = Response.Fail(
+        "Validation error",
+        errors);
+
+      context.Result = new BadRequestObjectResult(response);
+
+      context.ExceptionHandled = true;
+    }
+
+    private void HandleInvalidModelStateException(ExceptionContext context)
+    {
+      var response = Response.Fail(
+        string.Empty,
+        "Invalid model state");
+
+      context.Result = new BadRequestObjectResult(response);
+
+      context.ExceptionHandled = true;
+    }
+
+    private void HandleNotFoundException(ExceptionContext context)
+    {
+      var exception = context.Exception as NotFoundException;
+
+      var response = Response.Fail(
+        "The specified resource was not found.",
+        exception.Message);
+
+      context.Result = new NotFoundObjectResult(response);
+
+      context.ExceptionHandled = true;
+    }
+  }
 }
